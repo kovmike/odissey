dc801b63d916d887228f7013520c0800afec50b9
Михаил <kovmike@yandex.ru>
2021-12-05 10:06:06 +0300

    noGoal

---
+++ src/components/noGoalNotification.tsx
@@ -0,0 +1,29 @@
+import { Button } from "primereact/button";
+import { Dialog } from "primereact/dialog";
+import { useState } from "react";
+import { GoalBuilder } from "./goalBuilder";
+
+export const NoGoal: React.FC = () => {
+  const [showGoalBuilder, setShowGoalBuilder] = useState<boolean>(false);
+  return (
+    <div>
+      <span>
+        У тебя нет цели(как у общества без цветовой дифференциации штанов)
+      </span>
+      <Button
+        label="Создай цель"
+        onClick={() => {
+          setShowGoalBuilder(true);
+        }}
+      ></Button>
+      <Dialog
+        header="Задай цель"
+        visible={showGoalBuilder}
+        style={{ width: "50vw" }}
+        onHide={() => setShowGoalBuilder(false)}
+      >
+        <GoalBuilder />
+      </Dialog>
+    </div>
+  );
+};
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,22 +1,18 @@
 import { useState } from "react";
 import { useStore } from "effector-react";
 import { Button } from "primereact/button";
-import { Dialog } from "primereact/dialog";
-import { GoalBuilder } from "../../components/goalBuilder";
+
 import { $userGoal, testSet } from "../../features/goals/model";
+import { NoGoal } from "../../components/noGoalNotification";
 
 export const HomePage: React.FC<{ user: any }> = ({ user }) => {
   const goal = useStore($userGoal);
-  const [displayBasic, setDisplayBasic] = useState<boolean>(false);
 
   const prepareGoalData = (data: any) => {
     return data && data?.goal?.startWeight === 0 ? (
-      <div>
-        <span>Увас нет цели</span>
-        <Button onClick={() => setDisplayBasic(true)}>создать цель</Button>
-      </div>
+      <NoGoal />
     ) : (
-      <span>sdfsdf</span>
+      <span>goal</span>
     );
   };
 
@@ -26,15 +22,6 @@ export const HomePage: React.FC<{ user: any }> = ({ user }) => {
       <hr />
       {prepareGoalData(goal)}
       <Button onClick={() => testSet()}>test</Button>
-
-      <Dialog
-        header="Задай цель"
-        visible={displayBasic}
-        style={{ width: "50vw" }}
-        onHide={() => setDisplayBasic(false)}
-      >
-        <GoalBuilder />
-      </Dialog>
     </div>
   );
 };
