a972a5a0779e80ed35ca7443540db80acad60b40
Михаил <kovmike@yandex.ru>
2021-12-25 15:33:42 +0300

    checkwidth

--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,8 +1,14 @@
+import { useEffect, useState } from "react";
 import { AuthForm } from "../../components/auth";
 
 export const Auth: React.FC = () => {
+  const [w, setW] = useState(0);
+  useEffect(() => {
+    setW(document.documentElement.clientWidth);
+  }, []);
   return (
-    <div className={'auth-form'}>
+    <div className={"auth-form"}>
+      {w}
       <AuthForm />
     </div>
   );
